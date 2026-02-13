480aaf8899e5bdc495322801e4562f6695a62e4f
VldMshn <vladmosna@gmail.com>
2026-02-13 18:04:41 +0200

    update

---
+++ main.py
@@ -0,0 +1,53 @@
+# бей ближнего сри на нижнего
+
+# Тип данних - характеристика данних що обозначає діапазон значень то набір операцій
+
+collection = list() # Функція коструктор
+collection = []
+
+print(type(collection)) # Класс list
+
+items = [10, 12.3, "text", True] # погана практика
+
+fruits = ["avocado", "apple", "orange", "lemon", "pear"]
+print(fruits)
+"""
+print(fruits[0])
+print(fruits[1])
+print(fruits[1:3])
+print(fruits[:3])
+print(fruits[2:])
+print(fruits[1:4:2])
+print(fruits[::2])
+
+print(fruits[-1])
+print(fruits[-1:-3:-2])
+print(fruits[::-2])
+
+
+string = "text"
+string[2] = "u"
+"""
+
+fruits[1] = "mango"
+print(fruits)
+
+fruits_count = len(fruits)
+print(fruits_count)
+
+counter = 0
+while counter < len(fruits):
+    print(fruits[counter])
+    counter += 1
+
+for fruit in fruits:
+    print(fruit)
+
+names = input("Введіть список імен через кому: ")
+print(type(names))
+names = names.split(", ")
+print(type(names))
+print(names)
+print(type(names))
+print(", ".join(names))
+print(type(names))
